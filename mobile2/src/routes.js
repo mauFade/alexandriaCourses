f3b1d3b88c12b45791f90dd21a2be9d62a573f38
Mauricio <mauricio.cds00@gmail.com>
2022-01-06 14:54:04 -0300

    adição do mobile

---
+++ mobile2/src/routes.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+import React from 'react';
+
+const AppStack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }} >
+                <AppStack.Screen name='Incidents' component={Incidents} />
+                <AppStack.Screen name='Detail' component={Detail} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
