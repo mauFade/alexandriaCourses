4d105dc3570bd28d19cb0fcfbc31efa40a8b275e
Mauricio Pereira Cardoso <85966442+mauFade@users.noreply.github.com>
2022-01-07 09:02:09 -0300

    Update index.js

--- frontend/src/components/NewIncident/index.js
+++ frontend/src/components/NewIncident/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './style.css';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
 
 const NewIncident = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      })
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente.');
+    }
+  }
     return (
         <div className='new-incident-container'>
             <div className='content'>
@@ -18,9 +48,9 @@ const NewIncident = () => {
                     </Link>
                 </session>
                 <form>
-                    <input placeholder='Título do caso'/>
-                    <textarea placeholder='Descrição' />
-                    <input placeholder='Valor em reais'/>
+                    <input placeholder='Título do caso' value={title} onChange={e => setTitle(e.target.value)} />
+                    <textarea placeholder='Descrição' value={description} onChange={e => setDescription(e.target.value)} />
+                    <input placeholder='Valor em reais' value={value} onChange={e => setValue(e.target.value)} />
                     <button className='button' type='submit'>Cadastrar</button>
                 </form>
             </div>
